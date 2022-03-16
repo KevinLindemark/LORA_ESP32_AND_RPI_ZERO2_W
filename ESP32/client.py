0f23c780ba745b3ff02d0fe554648d36de8c89ef
KevinLindemark <58036568+KevinLindemark@users.noreply.github.com>
2022-03-16 22:59:18 +0100

    Add files via upload

--- ESP32/client.py
+++ ESP32/client.py
@@ -1,7 +1,18 @@
 from time import sleep
 from ulora import LoRa, ModemConfig, SPIConfig
 # based on https://github.com/martynwheeler/u-lora
+
+# PINOUT 
+# RFM9X <--> ESP32 WROOM-32D
+# VIN <--> 3v3
+# GND <--> GND
+# G0 <--> GPIO 0 (CAN BLOCK FROM FILESYSTEM ESP32 WHILE INSERTED)
+# SCK <--> GPIO 14
+# MISO <--> GPIO 12
+# MOSI <--> GPIO 13
+# CS <--> GPIO 5
 # Lora Parameters
+
 RFM95_RST = 27
 RFM95_SPIBUS = SPIConfig.esp32_1
 RFM95_CS = 5
